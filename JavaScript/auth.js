b729edf7381784255fa4dbde4bb02cc3876eadcc
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2026-01-08 09:45:05 +0500

    Electronic library

---
+++ JavaScript/auth.js
@@ -0,0 +1,41 @@
+// SignUp uchun JS
+document.getElementById('signupForm')?.addEventListener('submit', function (e) {
+	e.preventDefault()
+
+	const inputs = this.querySelectorAll('input')
+	const user = {
+		name: inputs[0].value,
+		email: inputs[1].value,
+		password: inputs[2].value,
+	}
+
+	localStorage.setItem('user', JSON.stringify(user))
+	alert("Ro'yxatdan o'tish muvaffaqiyatli!")
+	window.location.href = 'login.html'
+})
+
+// Login uchun JS
+
+document.getElementById("loginForm")?.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  const inputs = this.querySelectorAll("input");
+  const email = inputs[0].value;
+  const password = inputs[1].value;
+
+  const savedUser = JSON.parse(localStorage.getItem("user"));
+
+  if(!savedUser){
+    alert("Avval ro'yxatdan o'ting!");
+    return;
+  }
+
+  if(email === savedUser.email && password === savedUser.password){
+    localStorage.setItem("isLoggedIn", "true");
+    alert("Kirish muvaffaqiyatli!");
+    window.location.href = "bosh_sahifa.html";
+  } else {
+    alert("Email yoki parol noto‘g‘ri!");
+  }
+});
+
